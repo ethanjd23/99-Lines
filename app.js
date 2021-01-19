2c0df45f0a770ab62d76d4e2482fbae03b41f835
ethanjd23 <ethanjd23@gmail.com>
2021-01-19 14:52:21 -0600

    Only running first item in array

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-friends = ["Abbie", 'Emily', 'Mordecai', 'Bethaney', 'Lindsey'];
-let lineNumber = 99;
+friends = ["Abbie", 'Emily', 'Mordecai', 'Bethaney', 'Lindsey']
+lineNumber = 100
 for (let i = 0; i < friends.length; i++) {
-    while (lineNumber > 1) {
-        console.log(lineNumber + " lines of code in the file, " + lineNumber + " lines of code; " + friends[i] +' strikes one out, clears it all out, ' + lineNumber + " lines of code in the file.")
-        lineNumber++}
-        if (lineNumber == 1) {
-            console.log(lineNumber + " line of code in the file, " + lineNumber + " line of code; " + friends[i] +' strikes one out, clears it all out, ' + lineNumber + " line of code in the file.")
-        }
-    }
+    while (lineNumber > 0) {
+       lineNumber--;
+       if (lineNumber == 1) {
+        console.log(lineNumber + " line of code in the file, " + lineNumber + " line of code; " + friends[i] +' strikes one out, clears it all out, ' + lineNumber + " line of code in the file.");
+        continue;
+       };
+       console.log(lineNumber + " lines of code in the file, " + lineNumber + " lines of code; " + friends[i] +' strikes one out, clears it all out, ' + lineNumber + " lines of code in the file.");
+    };
+};
+    
+    
